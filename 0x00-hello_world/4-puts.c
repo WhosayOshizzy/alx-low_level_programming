b3cf03410dbd3f0647bcd9f5b4810a37e82e30c4
WhosayOshizzy <fabulousoshi@gmail.com>
2023-03-18 17:22:57 -0700

    C hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like buildig a multui ligual puzzle");
+	return (0);
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	pritf("Size of char: %i byte(s)\n", sizeof{char));
+	pritf("Size of char: %i byte(s)\n", sizeof{int));
+	pritf("Size of char: %i byte(s)\n", sizeof{long int));
+	pritf("Size of char: %i byte(s)\n", sizeof{long long int));
+	pritf("Size of char: %i byte(s)\n", sizeof{float));
+		return (0);
