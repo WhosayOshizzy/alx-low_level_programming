dea92df4cb7052e00f204ebd529e76cebd376379
WhosayOshizzy <fabulousoshi@gmail.com>
2023-03-18 20:07:38 -0700

    C hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry
- * Return: Always 0
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("\"Programming is like buildig a multui ligual puzzle");
+	puts("\"Programming is like buildig a multilingual puzzle");
 	return (0);
+}
