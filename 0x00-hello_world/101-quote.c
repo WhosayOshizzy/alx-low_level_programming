c0d34cb599f6d1446f40ae2e6f9aeec7a2bdba00
WhosayOshizzy <fabulousoshi@gmail.com>
2023-03-19 06:03:17 -0700

    C hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - Prints "and that piece of art is useful" -  Dora Korpar, 2015-10-19", followed by a new line, to standard error. 
- *
- * Return: Always 1.
- */
+#include <stdio.h>
 
+/**
+ * main - This program prints a sentence and a date, followed
+ * by a new line, to the standard error
+ * Return: 1 for success
+ */
 int main(void)
 {
-	write (2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
